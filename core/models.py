dcea5df459a6640f89bca6d31a9accddf56c0493
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-05-13 04:41:00 -0700

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,1 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
 
+class Link(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.URLField()
+    submitted_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
