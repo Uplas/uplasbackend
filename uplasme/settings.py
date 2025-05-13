229decd4d8f3e5914c5b62e6f2e575f670335db6
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-05-13 04:30:48 -0700

    Update settings.py

--- uplasme/settings.py
+++ uplasme/settings.py
@@ -5,11 +5,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
 ]
