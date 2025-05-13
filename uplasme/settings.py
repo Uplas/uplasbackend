db9cd9e256ba68b10fb6b6b117d6ada32325b110
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-05-13 03:46:22 -0700

    Update settings.py

--- uplasme/settings.py
+++ uplasme/settings.py
@@ -1,9 +1,9 @@
- INSTALLED_APPS = [
-   'django.contrib.admin',
-   'django.contrib.auth',
-   'django.contrib.contenttypes',
-   'django.contrib.sessions',
-   'django.contrib.messages',
-   'django.contrib.staticfiles',
-   'core',
- ]
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'core',  # <--- Make absolutely sure this line is here and correct
+]
