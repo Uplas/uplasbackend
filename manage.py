8befdde487b57e40098bd7c27614cdc7acff2f16
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-05-19 08:28:42 -0700

    Delete manage.py

--- manage.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-# Add the 'backend' directory to the Python path
-BACKEND_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(BACKEND_DIR)
-
-import django
-from django.core.management import execute_from_command_line
