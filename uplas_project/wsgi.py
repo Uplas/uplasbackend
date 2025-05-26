0fd60a225fca8ea72a4a14a04718c95d452ce709
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-05-26 06:14:15 -0700

    Update wsgi.py

--- uplas_project/wsgi.py
+++ uplas_project/wsgi.py
@@ -1,1 +1,9 @@
 
+"""
+WSGI config for uplas_project project.
+"""
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uplas_project.settings')
+application = get_wsgi_application()
