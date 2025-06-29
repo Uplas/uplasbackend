be32ec3658f2857afe59097c4d8b14bfaad344fe
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-06-29 03:25:27 -0700

    Update urls.py

--- uplas_project/urls.py
+++ uplas_project/urls.py
@@ -1,21 +1,26 @@
+# uplas/urls.py
+"""
+URL configuration for the Uplas project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from apps.core.views import api_root
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', api_root, name='api-root-default'),
-    path('api/core/', include('apps.core.urls', namespace='core')),
-    path('api/users/', include('apps.users.urls', namespace='users')),
-    path('api/courses/', include('apps.courses.urls', namespace='courses')),
-    path('api/payments/', include('apps.payments.urls', namespace='payments')),
-    path('api/projects/', include('apps.projects.urls', namespace='projects')),
-    path('api/community/', include('apps.community.urls', namespace='community')),
-    path('api/blog/', include('apps.blog.urls', namespace='blog')),
-    path('api/ai/', include('apps.ai_agents.urls', namespace='ai_agents')),
+    path('api/auth/', include('users.urls')),
+    path('api/payments/', include('payments.urls')),
+    path('api/', include('courses.urls')), # Includes both /courses and /team
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
